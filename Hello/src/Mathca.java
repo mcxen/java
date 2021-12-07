ac3c36614bc5b0c401a10896a4f06b8af74a9439
mcxen <83444002+mcxen@users.noreply.github.com>
2021-12-07 11:36:04 +0800

    Add files via upload

---
+++ Hello/src/Mathca.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class Mathca {
+
+    public List<String> generateParenthesis(int n) {
+        _generate(0,0,n,"");
+        return null;
+    }
+
+    private void _generate(int left,int right, int n, String s) {
+        //terminator
+        if(left==n && right == n) {
+            System.out.println(s);
+            return;
+        }
+        //process
+        String s1=s+"(";
+        String s2=s+")";
+        //drill down
+        if(left<n) {
+            _generate(left + 1, right,n, s1);
+        }
+        if(left>right) {
+            _generate(left,right + 1, n, s2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Mathca sol=new Mathca();
+        System.out.println(sol.generateParenthesis(3));
+        System.out.println("Hello World");
+
+    }
+}
---
+++ Hello/src/aries.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class aries {
+    public static void main(String[] args) {
+        File myAries=new File("aries.txt");
+        try (Scanner myReader = new Scanner(myAries)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        }catch (FileNotFoundException e) {
+            System.out.println(myAries.getName()+" 未找到");
+            e.printStackTrace();
+        }
+    }
+}
